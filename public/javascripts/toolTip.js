cf62cda0c4694488ca2d9d3e6b8fed5120296244
Sebastian Ojefors <godnoken@gmail.com>
2022-06-10 22:42:04 +0200

    Add bought/crafted info to iteminstances

--- public/javascripts/toolTip.js
+++ public/javascripts/toolTip.js
@@ -11,11 +11,46 @@ function readItemInfo(event, list) {
     const emeraldImg = document.createElement("img");
     const inventor = document.createElement("p");
     const type = document.createElement("p");
+    const acquired = document.createElement("p");
 
     toolTipContainer.classList.add("tool-tip-container");
     name.classList.add("tool-tip-name");
     valueContainer.classList.add("tool-tip-value-container");
     emeraldImg.classList.add("tool-tip-emerald");
+
+    let item;
+    let itemInstanceInfo;
+    
+    if (list === itemList) {
+        item = list.find((item) => item._id === event.target.dataset.id);
+    } else {
+        list.find((itemInstance) => {
+            if (itemInstance._id === event.target.dataset.id) {
+                itemInstanceInfo = itemInstance;
+                item = itemList.find((item) => {
+                    if (item._id === itemInstance.item) return item;
+                });
+            }
+        });
+    }
+    
+    
+    toolTipContainer.style.left = event.x + 15 + "px";
+    toolTipContainer.style.top = event.y + "px";
+    
+    name.textContent = item.name;
+    description.textContent = item.description;
+    value.textContent = item.value;
+    inventor.textContent = item.inventor.name;
+    type.textContent = item.type[0].name;
+    if (list === itemInstancesList) {
+        let itemAcquired = Math.abs(Math.ceil((new Date(itemInstanceInfo.acquired).getTime() - new Date().getTime()) / (1000 * 3600 * 24)));
+        acquired.textContent = `${itemInstanceInfo.crafted ? "Crafted: " : "Bought: "} ${itemAcquired} ${itemAcquired === 1 ? "day ago" : "days ago"}`;
+    }
+
+
+    emeraldImg.srcset = "images/emerald.webp";
+    emeraldImg.src = "images/emerald.png";
 
     toolTipContainer.appendChild(name);
     toolTipContainer.appendChild(description);
@@ -24,32 +59,7 @@ function readItemInfo(event, list) {
     valueContainer.appendChild(emeraldImg);
     toolTipContainer.appendChild(type);
     toolTipContainer.appendChild(inventor);
+    toolTipContainer.appendChild(acquired);
     document.body.appendChild(toolTipContainer);
-
-    let item;
-
-    if (list === itemList) {
-      item = list.find((item) => item._id === event.target.dataset.id);
-    } else {
-        list.find((itemInstance) => {
-            if (itemInstance._id === event.target.dataset.id) {
-                item = itemList.find((item) => {
-                    if (item._id === itemInstance.item) return item;
-                });
-            }
-        });
-    }
-
-    toolTipContainer.style.left = event.x + 15 + "px";
-    toolTipContainer.style.top = event.y + "px";
-
-    name.textContent = item.name;
-    description.textContent = item.description;
-    value.textContent = item.value;
-    inventor.textContent = item.inventor.name;
-    type.textContent = item.type[0].name;
-
-    emeraldImg.srcset = "images/emerald.webp";
-    emeraldImg.src = "images/emerald.png";
   }
 }
